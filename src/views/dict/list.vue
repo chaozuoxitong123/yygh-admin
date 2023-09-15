494a710c330c31367b852378f2046c65cce66a2e
老马 <3314991589@qq.com>
2023-09-15 19:01:19 +0800

    完成数据字典界面的导入功能，但是导入的数据不能在页面显示，需要解决

--- src/views/dict/list.vue
+++ src/views/dict/list.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="app-container">
+<!--    导出和导入按钮-->
     <div class="el-toolbar">
       <div class="el-toolbar-body" style="justify-content: flex-start;">
         <a href="http://localhost:8202/admin/cmn/dict/exportData" target="_blank">
           <el-button type="text"><i class="fa fa-plus"/> 导出</el-button>
         </a>
+          <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
       </div>
     </div>
+
     <el-table
       :data="list"
       style="width: 100%"
@@ -37,6 +40,29 @@
         </template>
       </el-table-column>
     </el-table>
+
+<!--    导入的弹框-->
+    <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+      <el-form label-position="right" label-width="170px">
+        <el-form-item label="文件">
+          <el-upload
+            :multiple="false"
+            :on-success="onUploadSuccess"
+            :action="'http://localhost:8202/admin/cmn/dict/importData'"
+            class="upload-demo">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传xls文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogImportVisible = false">
+          取消
+        </el-button>
+      </div>
+    </el-dialog>
+
+
   </div>
 </template>
 
@@ -46,6 +72,7 @@ import dict from '@/api/dict'
 export default {
   data() {
     return {
+      dialogImportVisible:false,//设置弹出框是否弹出
       list:[] //数据字典列表数组
     }
   },
@@ -53,6 +80,17 @@ export default {
     this.getDictList(1)
   },
   methods: {
+    //上传成功调用
+    onUploadSuccess(){
+      //关闭弹框
+      this.dialogImportVisible = false
+      //刷新页面
+      this.getDictList(1)
+    },
+    //数据字典导入
+    importData(){
+      this.dialogImportVisible = true
+    },
     //数据字典导出
     exportData() {
       window.location.href = 'http://localhost:8202/admin/cmn/dict/exportData'
